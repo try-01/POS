440b34fe834cbd4acc7f8f36995bd8cd80fd1f2d
b3bas <benas@gmail.com>
2026-07-09 13:23:54 +0700

    update

--- app/src/main/java/com/example/kasirpos/ui/inventory/InventoryScreen.kt
+++ app/src/main/java/com/example/kasirpos/ui/inventory/InventoryScreen.kt
@@ -14,8 +14,8 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardOptions
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
