a30f9ceb5ea57c9b5531bfbc35678a27231339ee
b3bas <benas@gmail.com>
2026-07-17 19:58:23 +0700

    update

--- app/src/main/java/com/pos/offline/MainActivity.kt
+++ app/src/main/java/com/pos/offline/MainActivity.kt
@@ -29,6 +29,7 @@ import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Assessment
 import androidx.compose.material.icons.rounded.Inventory2
