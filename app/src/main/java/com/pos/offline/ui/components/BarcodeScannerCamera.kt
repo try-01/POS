60e66c7cdf09e99cee68e97db201724040d4cb88
b3bas <benas@gmail.com>
2026-07-18 18:32:13 +0700

    update

--- app/src/main/java/com/pos/offline/ui/components/BarcodeScannerCamera.kt
+++ app/src/main/java/com/pos/offline/ui/components/BarcodeScannerCamera.kt
@@ -15,10 +15,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.compose.LocalLifecycleOwner
-import com.google.mlkit.vision.barcode.Barcode
+import com.google.mlkit.vision.barcode.common.Barcode
 import com.google.mlkit.vision.barcode.BarcodeScannerOptions
 import com.google.mlkit.vision.barcode.BarcodeScanning
 import com.google.mlkit.vision.common.InputImage
