60d52524e9e7c20a694cb61001e3def025139c0d
b3bas <benas@gmail.com>
2026-07-25 21:59:12 +0700

    update

--- app/src/main/java/com/pos/offline/MainActivity.kt
+++ app/src/main/java/com/pos/offline/MainActivity.kt
@@ -23,8 +23,10 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.ime
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.navigationBarsPadding
