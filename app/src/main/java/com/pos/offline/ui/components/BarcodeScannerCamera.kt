7194bcd8302873a1f8cf544ba41739c387a064a0
b3bas <benas@gmail.com>
2026-07-28 20:23:44 +0700

    update

--- app/src/main/java/com/pos/offline/ui/components/BarcodeScannerCamera.kt
+++ app/src/main/java/com/pos/offline/ui/components/BarcodeScannerCamera.kt
@@ -33,6 +33,7 @@ import androidx.compose.material.icons.rounded.Close
 import androidx.compose.material.icons.rounded.QrCodeScanner
 import androidx.compose.material.icons.rounded.Repeat
 import androidx.compose.material.icons.rounded.Warning
+import androidx.compose.material3.Surface
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
