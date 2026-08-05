69b920858d7a36005e6b60e06294951f9cff74c6
b3bas <benas@gmail.com>
2026-08-05 23:53:22 +0700

    update

--- coba/build.gradle.kts
+++ coba/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.application") version "9.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.3.20" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.3.20" apply false
     id("com.google.devtools.ksp") version "2.3.11" apply false
     id("androidx.room3") version "3.0.1" apply false
